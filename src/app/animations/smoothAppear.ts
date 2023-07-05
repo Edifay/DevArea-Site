476b4468683a718b0adb43704a2c4e241a10ef25
Edifay <59512940+Edifay@users.noreply.github.com>
2023-07-05 16:28:20 +0200

    little changes

--- src/app/animations/smoothAppear.ts
+++ src/app/animations/smoothAppear.ts
@@ -1,7 +1,6 @@
 import {animate, state, style, transition, trigger} from "@angular/animations";
 
 export const smoothAppear = trigger('appear', [
-    state('in', style({transform: 'translateX(0)'})),
     transition('void => *', [
         style({opacity: 0}),
         animate(200)
--- src/app/components/pages/options/components/personnal-xp/personnal-xp.ts
+++ src/app/components/pages/options/components/personnal-xp/personnal-xp.ts
@@ -1,23 +1,27 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-personnal-xp',
-  templateUrl: './personnal-xp.html',
-  styleUrls: ['./personnal-xp.scss']
+    selector: 'app-personnal-xp',
+    templateUrl: './personnal-xp.html',
+    styleUrls: ['./personnal-xp.scss'],
 })
 export class PersonnalXp implements OnInit {
-  @Input() avatar: string = "https://www.magimix.com/webroot-mobile/img/loading.gif";
-  @Input() xp: number = 0;
-  @Input() previous_level: number = 0;
-  @Input() next_level: number = 0;
-  @Input() name: string = "name";
-  @Input() previous_level_xp: number = 0;
-  @Input() next_level_xp: number = 0;
+    @Input() avatar: string = "https://www.magimix.com/webroot-mobile/img/loading.gif";
+    @Input() xp: number = 0;
+    @Input() previous_level: number = 0;
+    @Input() next_level: number = 0;
+    @Input() name: string = "name";
+    @Input() previous_level_xp: number = 0;
+    @Input() next_level_xp: number = 0;
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    getLong(): number {
+        return (this.xp - this.previous_level_xp) / (this.next_level_xp - this.previous_level_xp) * 100;
+    }
 
 }
